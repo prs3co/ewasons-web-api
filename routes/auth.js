585e2da1e3c6234c8097b0f8fa5a1c36eb35f4d0
Pr <73705323+prs3co@users.noreply.github.com>
2022-12-03 01:45:47 +0800

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -22,7 +22,7 @@ router.post('/register', async (req, res) => {
 // login
 router.post('/login', async (req, res) => {
   try {
-    const user = await User.findOne({ username: req.body.username })
+    const user = await User.findOne({ email: req.body.email })
 
     // !user && res.status(401).json("Wrong credentials!")
     //
