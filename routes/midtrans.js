c10484c4f8bedc9511e97a41757634861c6a6805
Pr <73705323+prs3co@users.noreply.github.com>
2022-12-11 10:22:48 +0800

    "fixing routing midtrans"

--- routes/midtrans.js
+++ routes/midtrans.js
@@ -1,173 +1,40 @@
-const axios = require('axios')
-
-// const midtransClient = require('midtrans-client')
+const router = require('express').Router()
+const midtransClient = require('midtrans-client')
 
 const getCurrentTimestamp = () => {
   return '' + Math.round(new Date().getTime() / 1000)
 }
 
-const handleMainRequest = (req, res) => {
-  // Perform Snap API request
-  axios({
-    // Below is the API URL endpoint
-    url: 'https://app.midtrans.com/snap/v1/transactions',
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      Authorization:
-      `Basic ${Buffer.from(process.env.MIDTRANS_KEY).toString('base64')}`
-      // 'Basic ' +
-      // Buffer.from('SB-Mid-server-GwUP_WGbJPXsDzsNEBRs8IYA').toString('base64')
-      // Above is API server key for the Midtrans account, encoded to base64
-    },
-    data:
-      // Below is the HTTP request body in JSON
-      {
-        transaction_details: {
-          order_id: `CustOrder-102${getCurrentTimestamp()}`,
-          gross_amount: 13000
-        },
-        credit_card: {
-          secure: true
-        },
-        item_details: [
-          {
-            id: 'a01',
-            price: 7000,
-            quantity: 1,
-            name: 'Kabel Bekas'
-          },
-          {
-            id: 'b02',
-            price: 3000,
-            quantity: 2,
-            name: 'Lampu Bekas'
-          }
-        ],
-        customer_details: {
-          first_name: 'Budi',
-          last_name: 'Susanto',
-          email: 'budisusanto@example.com',
-          phone: '+628123456789',
-          billing_address: {
-            first_name: 'Budi',
-            last_name: 'Susanto',
-            email: 'budisusanto@example.com',
-            phone: '08123456789',
-            address: 'Sudirman No.12',
-            city: 'Jakarta',
-            postal_code: '12190',
-            country_code: 'IDN'
-          },
-          shipping_address: {
-            first_name: 'Budi',
-            last_name: 'Susanto',
-            email: 'budisusanto@example.com',
-            phone: '0812345678910',
-            address: 'Sudirman',
-            city: 'Jakarta',
-            postal_code: '12190',
-            country_code: 'IDN'
-          }
-        }
-      }
-  }).then(
-    (snapResponse) => {
-      const snapToken = snapResponse.data.token
-      const snapUrl = snapResponse.data.redirect_url
-      // console.log('Retrieved snap token:', snapToken)
-      // Pass the Snap Token to frontend, render the HTML page
-      res.status(200).json({ snapToken, snapUrl })
-    },
-    (error) => {
-      res.status(500).json(error)
-      console.log(error)
-    }
-  )
-}
-const handleTestRequest = (req, res) => {
-  // Perform Snap API request
-  axios({
-    // Below is the API URL endpoint
-    url: 'https://app.sandbox.midtrans.com/snap/v1/transactions',
-    method: 'post',
-    headers: {
-      'Content-Type': 'application/json',
-      Accept: 'application/json',
-      Authorization:
-      // `Basic ${Buffer.from(process.env.MIDTRANS_KEY).toString('base64')}`
-      'Basic ' +
-      Buffer.from(process.env.MIDTRANS_SB_KEY).toString('base64')
-      // Above is API server key for the Midtrans account, encoded to base64
-    },
-    data:
-      // Below is the HTTP request body in JSON
-      {
-        transaction_details: {
-          order_id: `CustOrder-102${getCurrentTimestamp()}`,
-          gross_amount: 13000
-        },
-        credit_card: {
-          secure: true
-        },
-        item_details: [
-          {
-            id: 'a01',
-            price: 7000,
-            quantity: 1,
-            name: 'Kabel Bekas'
-          },
-          {
-            id: 'b02',
-            price: 3000,
-            quantity: 2,
-            name: 'Lampu Bekas'
-          }
-        ],
-        customer_details: {
-          first_name: 'Budi',
-          last_name: 'Susanto',
-          email: 'budisusanto@example.com',
-          phone: '+628123456789',
-          billing_address: {
-            first_name: 'Budi',
-            last_name: 'Susanto',
-            email: 'budisusanto@example.com',
-            phone: '08123456789',
-            address: 'Sudirman No.12',
-            city: 'Jakarta',
-            postal_code: '12190',
-            country_code: 'IDN'
-          },
-          shipping_address: {
-            first_name: 'Budi',
-            last_name: 'Susanto',
-            email: 'budisusanto@example.com',
-            phone: '0812345678910',
-            address: 'Sudirman',
-            city: 'Jakarta',
-            postal_code: '12190',
-            country_code: 'IDN'
-          }
-        }
-      }
-  }).then(
-    (snapResponse) => {
-      const snapToken = snapResponse.data.token
-      const snapUrl = snapResponse.data.redirect_url
-      // console.log('Retrieved snap token:', snapToken)
-      // Pass the Snap Token to frontend, render the HTML page
-      res.status(200).json({ snapToken, snapUrl })
-    },
-    (error) => {
-      res.status(500).json(error)
-      console.log(error)
-    }
-  )
-}
+// create
+router.post('/', async (req, res) => {
+  const snap = new midtransClient.Snap({
+    // Set to true if you want Production Environment (accept real transaction).
+    isProduction: true,
+    serverKey: process.env.MIDTRANS_KEY
+  })
 
-module.exports = {
-  handleMainRequest,
-  handleTestRequest
-}
+  const parameter = {
+    transaction_details: {
+      order_id: `CustOrder-82${getCurrentTimestamp()}`,
+      gross_amount: req.body.amount
+    },
+    credit_card: {
+      secure: true
+    },
+    customer_details: {
+      username: req.body.username,
+      email: req.body.email
+    }
+  }
+
+  snap.createTransaction(parameter)
+    .then((transaction) => {
+      // const snapToken = snapResponse.data.token
+      // const snapUrl = snapResponse.data.redirect_url
+      // transaction token
+      const transactionToken = transaction.token
+      res.status(200).send({ transactionToken })
+    })
+})
+
+module.exports = router
