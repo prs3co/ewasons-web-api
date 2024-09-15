5378e82515e0bc81ed15fec566a8f9c510fb39a7
unknown <erdonauz@gmail.com>
2024-09-15 17:17:28 +0800

    fix: fix ping route

--- routes/ping.js
+++ routes/ping.js
@@ -1,23 +1,22 @@
-const User = require('../models/User')
 const mongoose = require('mongoose')
 const router = require('express').Router()
 
 const formatDate = (date) => {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
-  const day = String(date.getDate()).padStart(2, '0');
-  return `${day}-${month}-${year}`;
-};
+  const year = date.getFullYear()
+  const month = String(date.getMonth() + 1).padStart(2, '0')
+  const day = String(date.getDate()).padStart(2, '0')
+  return `${day}-${month}-${year}`
+}
 
 router.get('/heartbeat', async (req, res) => {
   const now = new Date()
   const currentTime = formatDate(now)
   try {
-    await mongoose.connection.db.command({ ping: 1 });
-    res.send(`DB Server is running ${currentTime}`);
+    await mongoose.connection.db.command({ ping: 1 })
+    res.send(`DB Server is running ${currentTime}`)
   } catch (error) {
-    res.send(`DB Server is down`);
+    res.send(`DB Server is down: ${error}`)
   }
-});
+})
 
 module.exports = router
